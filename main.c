a1775534d0ec1b29b1580c07ebf61837c5b950a4
Daniel300 <daniel@causebrook.co.uk>
2015-09-29 07:16:40 +0100

    Added code so user can choose a method to use.

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+long double interval_bisection(char eqn,int iterations,int interval) {
+
+}
+
+long double eval_func(char eqn, int x) {
+	
+}
+
 int main()
 {
-	const int ADD = 5;
-    int integer = 10;
-    integer += ADD;
-    printf("Sum of values: %d \n",integer);
+	char str[3];
+	str[0] = 0;
+	printf("Numerical Methods: \nIntervalBisection('IB')\nPlease enter a numerical method to use: ");
+	scanf("%s",str);
+	printf("Input: '%s'\n",str);
+	if (strcmp(str, "IB") == 0) {
+		printf("Interval Bisection\n");
+	}
     return 0;
 }
